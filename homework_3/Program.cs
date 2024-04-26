5e2d3b3c421edb98af03b28a6a46d6c5aba921bf
Vladimir <vlnshapat@gmail.com>
2024-04-26 22:16:21 +0300

    add homework_3

---
+++ homework_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//напишите метод , который на вход принимает число number и выводит,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите  число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number%2;
+
+if (result == 0)
+{
+   Console.WriteLine($"четное"); 
+}
+else
+{
+    Console.WriteLine($"нечетное");
+}
