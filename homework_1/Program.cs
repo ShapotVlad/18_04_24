28a4796999db84989727f07705bfa99e61f9afb6
Vladimir <vlnshapat@gmail.com>
2024-04-26 20:03:59 +0300

    add two variant

--- homework_1/Program.cs
+++ homework_1/Program.cs
@@ -1,4 +1,4 @@
-﻿//напишите метод, который на вход принимает два числа и выводит, какое число большее,
+﻿﻿//напишите метод, который на вход принимает два числа и выводит, какое число большее,
 //а какое меньшее.
 
 
@@ -20,15 +20,6 @@ else
 {
     Console.WriteLine($"второе число больше");
 }
-//напишите метод, который на вход принимает два числа и выводит, какое число большее,
-//а какое меньшее.
-
-
-Console.Write("Введите первое число ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите второе число ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 
 if (firstNumber == secondNumber)
@@ -43,4 +34,5 @@ if (firstNumber == secondNumber)
         {
             Console.WriteLine($"второе число больше");
         }
+
         
