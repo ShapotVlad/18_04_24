887bfa875efe73286838f753f8a4d42eeb98a543
Vladimir <vlnshapat@gmail.com>
2024-04-26 21:15:14 +0300

    add homework_2

---
+++ homework_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//напишите метод FindMax, который принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+int a = 44;
+int b = 78;
+int c = 16;
+
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
