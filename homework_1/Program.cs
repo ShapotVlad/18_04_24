377cbc2d5cf68b4ef6225e14339b06e22cc3f2c7
Vladimir <vlnshapat@gmail.com>
2024-04-26 15:07:20 +0300

    added homework_1

---
+++ homework_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//напишите метод, который на вход принимает два числа и выводит, какое число большее,
+//а какое меньшее.
+
+
+Console.Write("Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (firstNumber == secondNumber)
+        {
+            Console.WriteLine($"числа равны");
+        }
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"первое число больше`");
+        }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"второе число больше");
+        }
+        
