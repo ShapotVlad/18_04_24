f26b755e33589ed6eda9e45ee17b38b7644d03f2
Vladimir <vlnshapat@gmail.com>
2024-04-26 23:06:58 +0300

    add homework_4

---
+++ homework_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// напишите метод , которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно),
+//  после каждого числа должен быть знак пробела.
+
+Console.Write("Введите  число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
+Console.Write("=>");
+for (int i = 2; i <= number; i += 2)
+{
+    Console.Write($"{i}" + ",");
+}
+
+
+
